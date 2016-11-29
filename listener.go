3064c3a99442c2dd6ae0980e44cb0a8fe640ac2d
Ox Cart <ox.to.a.cart@gmail.com>
2016-11-28 20:11:48 -0600

    More stringent closing sequence

--- listener.go
+++ listener.go
@@ -1,9 +1,14 @@
 package cmux
 
 import (
+	"errors"
 	"github.com/xtaci/smux"
 	"net"
 	"sync"
+)
+
+var (
+	ErrClosed = errors.New("listener closed")
 )
 
 type ListenOpts struct {
@@ -41,6 +46,7 @@ func Listen(opts *ListenOpts) net.Listener {
 
 func (l *listener) listen() {
 	defer l.Close()
+	defer close(l.nextErr)
 	for {
 		conn, err := l.wrapped.Accept()
 		if err != nil {
@@ -78,7 +84,7 @@ func (l *listener) handleConn(conn net.Conn) {
 	for {
 		stream, err := session.AcceptStream()
 		if err != nil {
-			log.Errorf("Error creating multiplexed session: %v", err)
+			log.Debugf("Error creating multiplexed session, probably just means that the underlying connection was closed: %v", err)
 			return
 		}
 		l.nextConn <- &cmconn{
@@ -91,9 +97,15 @@ func (l *listener) handleConn(conn net.Conn) {
 
 func (l *listener) Accept() (net.Conn, error) {
 	select {
-	case conn := <-l.nextConn:
+	case conn, ok := <-l.nextConn:
+		if !ok {
+			return nil, ErrClosed
+		}
 		return conn, nil
-	case err := <-l.nextErr:
+	case err, ok := <-l.nextErr:
+		if !ok {
+			return nil, ErrClosed
+		}
 		return nil, err
 	}
 }
@@ -110,6 +122,17 @@ func (l *listener) Close() error {
 	l.sessions = nil
 	err := l.wrapped.Close()
 	l.closed = true
+	// Drain nextConn and nextErr
+drain:
+	for {
+		select {
+		case <-l.nextConn:
+		case <-l.nextErr:
+		default:
+			break drain
+		}
+	}
+	close(l.nextConn)
 	l.mx.Unlock()
 	return err
 }
